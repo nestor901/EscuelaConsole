bb8514d4ad8981c6567a5bdc7170d55a13edd041
Nestor Adonai Portillo <nestor901@outlook.com>
2021-01-15 16:16:45 -0600

    Etapa2

---
+++ Etapa2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Etapa2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
